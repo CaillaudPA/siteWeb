10cac57b80c0988c03bc52e3ba33914ad11a271b
CaillaudPA <caillaud.pierreantoine@yahoo.com>
2015-03-02 14:52:18 +0100

    hlhkjhjk

--- js/acceuil.js
+++ js/acceuil.js
@@ -1,3 +1,16 @@
 $('document').ready(function(){
-	
+	//Définir toutes les variables
+	var menuDerouler = $('#menuDerouler');
+
+	//Evenement
+	menuDerouler.click(function () {
+		// body...
+		{
+			$('#menuDeroulerSlide').slideToggle("slow");
+		}
+	})
+
+	//Fonction
+
+
 });
