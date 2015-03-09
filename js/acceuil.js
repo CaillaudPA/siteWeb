fac65e43434c877b35f64dd7444dcb8a288c14b5
BeaulieuLoic <beaulielloic@gmail.com>
2015-03-09 17:21:58 +0100

    l

--- js/acceuil.js
+++ js/acceuil.js
@@ -1,4 +1,12 @@
+hauteurImgPrincipalOrigine = 943;
+largeurImgPrincipalOrigine = 400;
+
 $('document').ready(function(){
+
+	$(window).resize(function() {
+		redimentionnerImage();
+	});
+
 
 	//jQuery en rapport avec le menu NAV
 	//Définir toutes les variables
@@ -15,18 +23,54 @@ $('document').ready(function(){
 	//Supprimer le float après le menu
 	$('nav').nextAll().css('clear', 'both');
 	
-
-
-
-
 });
 
 function afficherInputRecherche(){
 	$('#recherche').css('display', 'inline');
 	$('#rechercheDiv').css('display', 'inline');
+
+	document.getElementById("recherche").focus();
 }
 
 function enleverInputRecherche(){
 	$('#recherche').css('display', 'none');
 	$('#rechercheDiv').css('display', 'none');
+}
+
+function redimentionnerImage(){
+	newHeight = $(window).width();
+	newWidth = $(window).width();
+
+	if(newWidth > largeurImgPrincipalOrigine){
+		newWidth = largeurImgPrincipalOrigine;
+	}
+
+	if(newHeight > hauteurImgPrincipalOrigine){
+		newHeight = hauteurImgPrincipalOrigine;
+	}
+
+
+	alert(newWidth+" | "+newHeight);
+}
+
+
+
+function redimimage(largeur, hauteur, largeurc, hauteurc) {
+    coord=new Array(2);
+    ratio=hauteur/largeur;
+    //si l'image réelle est plus petite en largeur et hauteur
+    if (largeur < largeurc && hauteur < hauteurc) {
+        while (largeur < largeurc && hauteur < hauteurc) {
+            largeur++;
+            hauteur=Math.round(largeur*ratio);
+        }
+    } else {
+        while (largeur > largeurc || hauteur > hauteurc) {
+            largeur--;
+            hauteur=Math.round(largeur*ratio);
+        }
+    }
+    coord[0]=largeur;
+    coord[1]=hauteur;
+    return(coord);
 }
