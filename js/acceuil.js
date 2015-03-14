e9ee766ed223bb7b4ca2aefeab546454dc076ac7
CaillaudPA <caillaud.pierreantoine@yahoo.com>
2015-03-14 10:42:46 +0100

    jh

--- js/acceuil.js
+++ js/acceuil.js
@@ -7,31 +7,18 @@ $('document').ready(function(){
 		redimentionnerImage();
 	});
 
-
-	//jQuery en rapport avec le menu NAV
-	//Définir toutes les variables
-	var menuDerouler = $('#menuDerouler');
-
-	//Evenement
-	menuDerouler.click(function () {
-		// body...
-		{
-			$('#menuDeroulerSlide').slideToggle("slow");
-		}
-	})
-
-	//Supprimer le float après le menu
-	$('nav').nextAll().css('clear', 'both');
 	
 });
 
 function afficherInputRecherche(){
+	if ($('#recherche').css('display') == 'none') {
 	$('#recherche').css('display', 'inline');
 	$('#rechercheDiv').css('display', 'inline');
-
-	document.getElementById("recherche").focus();
+	$("recherche").focus();}
+	
 }
 
+// Inutile ?
 function enleverInputRecherche(){
 	$('#recherche').css('display', 'none');
 	$('#rechercheDiv').css('display', 'none');
